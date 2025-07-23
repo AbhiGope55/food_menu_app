278448cb732c99c7aed5923d5336e0cba2cd12fa
AbhiGope55 <abhishek789901@gmail.com>
2025-07-24 00:14:13 +0530

    This is third commit updated template

--- food/models.py
+++ food/models.py
@@ -6,4 +6,5 @@ class Item(models.Model):
         return self.item_name
     item_name=models.CharField(max_length=200)
     item_desc=models.CharField(max_length=200)
+    item_image=models.CharField(max_length=500,default="https://cdn.dribbble.com/userupload/22570626/file/original-379b4978ee41eeb352e0ddacbaa6df96.jpg")
     item_price=models.IntegerField()
--- food/urls.py
+++ food/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from food import views
+app_name='food'
 urlpatterns = [
    #food/
    path('',views.index,name="index"), 
    #food/1
-   path('<int:item_id>',views.detail,name="detail"),
+   path('<int:item_id>/',views.detail,name="detail"),
    path('item/',views.item,name="item"), 
 ]
